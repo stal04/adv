8575cfe3b46a763974671bbab44bab40fc95f089
Lenka Šťastná <lenka2277@gmail.com>
2018-04-01 00:31:49 +0200

    gui

--- adv/src/uiText/HomeController.java
+++ adv/src/uiText/HomeController.java
@@ -4,10 +4,13 @@ package uiText;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import logika.IHra;
+import logika.Prostor;
+import logika.Vec;
 import logika.Hra;
 
 
@@ -15,15 +18,19 @@ public class HomeController extends AnchorPane{
 	
 	@FXML private TextArea vystup;
 	@FXML private TextField vstupniText;
+	
+
 	private IHra hra;
 
 	@FXML public void odeslaniPrikazu() {
-		String prikaz = vstupniText.getText();
-		String odpoved = hra.zpracujPrikaz(prikaz);
+		String radek = vstupniText.getText();
+		String odpoved = hra.zpracujPrikaz(radek);
 		vystup.setText(odpoved);
 		vstupniText.setText("");
 	}
 	
+	
+
 
 
 
